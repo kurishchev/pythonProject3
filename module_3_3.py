0f1c7877e6e08c06c1da36c77631b4dd95fc8ca5
kurishchev <kurishchev@rambler.ru>
2024-07-16 17:31:31 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -2,5 +2,12 @@ def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
 
 
-values_list_2 = ['булка', 3.14 ]
-print_params(*values_list_2, 57)
+values_list = ['булка', 3.14, [5, True]]
+print_params(*values_list)
+
+print_params(b = 25)
+
+print_params(c = [1,2,3])
+
+values_dict = {'a':15, 'b':'norma', 'c':[5, 8, 111]}
+print_params(**values_dict)
